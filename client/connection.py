391860672e1db3744e79a654a06dd3fbb782ca35
Marcin Zatorski <mar3211233@gmail.com>
2019-12-28 02:03:02 +0100

    Handle move confirmation from the server

--- client/connection.py
+++ client/connection.py
@@ -1,6 +1,4 @@
-import re
 from PyQt5 import QtCore, QtNetwork
-import game
 
 class Connection:
     def __init__(self, hostname, port, client):
@@ -10,23 +8,6 @@ class Connection:
         self.tcp_socket.connectToHost(hostname, port)
         self.client = client
 
-    def handle_message(self, message):
-        match = re.match(r"^MOVE (\d+) (\d+)", message)
-        if match is not None:
-            row = int(match.group(1))
-            column = int(match.group(2))
-            print("move", row, column)
-            self.client.game.move_enemy(row, column)
-        elif message.startswith("GAME CREATED"):
-            if message.endswith("BLACK\n"):
-                self.client.game.player = game.Field.BLACK
-            else:
-                self.client.game.player = game.Field.WHITE
-        elif message == "INVALID MESSAGE\n":
-            print("Error: client sent invalid message")
-        else:
-            print("Error: Invalid message")
-
     def send(self, data):
         self.tcp_socket.write(QtCore.QByteArray(bytes(data, encoding = "UTF-8")))
 
@@ -34,7 +15,7 @@ class Connection:
         data = self.tcp_socket.readLine()
         while not data.isEmpty():
             print(str(data.data(), "UTF-8"))
-            self.handle_message(str(data.data(), encoding = "UTF-8"))
+            self.client.handle_message(str(data.data(), encoding = "UTF-8"))
             data = self.tcp_socket.readLine()
 
     def __del__(self):
--- client/game.py
+++ client/game.py
@@ -1,3 +1,4 @@
+import re
 from enum import Enum
 from connection import Connection
 
@@ -10,8 +11,32 @@ class Client:
         self.game = Game(19, self.view)
         self.game.player = Field.BLACK
 
+    def handle_message(self, message):
+        if message.startswith("MOVE "):
+            match = re.match(r"^MOVE (\d+) (\d+)", message)
+            if match is not None:
+                row = int(match.group(1))
+                column = int(match.group(2))
+                print("move", row, column)
+                self.game.move_enemy(row, column)
+            elif message.endswith("OK\n"):
+                self.game.next_move = (-1, -1)
+            elif message.endswith("INVALID\n"):
+                pass
+            else:
+                print("Error: Invalid move message")
+        elif message.startswith("GAME CREATED"):
+            if message.endswith("BLACK\n"):
+                self.game.player = Field.BLACK
+            else:
+                self.game.player = Field.WHITE
+        elif message == "INVALID MESSAGE\n":
+            print("Error: client sent invalid message")
+        else:
+            print("Error: Invalid message")
+
     def move(self, row, column):
-        self.game.move_player(row, column)
+        self.game.set_next_move(row, column)
         self.connection.send("MOVE " + str(row) + " " + str(column) + "\n")
 
     def new_game(self):
@@ -26,6 +51,7 @@ class Game:
         self.board_size = board_size
         self.board = [[Field.NONE for i in range(board_size)] for i in range(board_size)]
         self.player = Field.NONE
+        self.next_move = (-1, -1)
         self.view = view
         self.view.update(self)
 
@@ -36,6 +62,16 @@ class Game:
     def move_enemy(self, row, column):
         self.board[row][column] = self.player.other()
         self.view.update(self)
+
+    def set_next_move(self, row, column):
+        if self.next_move[0] >= 0:
+            self.board[self.next_move[0]][self.next_move[1]] = Field.NONE
+            self.view.update(self)
+        if row >= 0 and self.board[row][column] == Field.NONE:
+            self.next_move = (row, column)
+            self.move_player(row, column)
+        else:
+            self.next_move = (-1, -1)
 
 class Field(Enum):
     """Enum class representing field on the board"""
